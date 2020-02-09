9cf4ef7aadecbd825ec3421e6fd83c7552f97b34
Sergey111991KV <sergey.cosilov@gmail.com>
2020-02-09 14:04:01 +0300

    kk

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,19 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
 import Control.Concurrent.Async
+import           Data.Aeson            (Value)
+import qualified Data.ByteString.Char8 as S8
+import qualified Data.Yaml             as Yaml
+import           Network.HTTP.Simple
+
 
 main :: IO ()
 main = do
-        putStrLn "someFunc"
-           
+    response <- httpJSON "https://api.telegram.org/bot<823135486:AAHRRGLZFyKAa-hh7bkULT_fLJEn_2xorpg>/"
+    
+    putStrLn $ "The status code was: " ++
+               show (getResponseStatusCode response)
+    print $ getResponseHeader "Content-Type" response
+    S8.putStrLn $ Yaml.encode (getResponseBody response :: Value)
--- src/Examples/Telegram/Internal/Requests.hs
+++ src/Examples/Telegram/Internal/Requests.hs
@@ -1,46 +1,48 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module Examples.Telegram.Internal.Requests
-  ( sendTelegram
-  ) where
+  -- ( sendTelegram
+  -- ) 
+  where
 
 import Data.ByteString.Char8 (pack)
 import Data.ByteString.Lazy (ByteString)
 import Examples.Requests.RequestBuilders
-import Logging.Config
+-- import Logging.Config
 import Network.HTTP.Client
 import Network.HTTP.Client.TLS (tlsManagerSettings)
 import qualified Network.HTTP.Types as HTTP
 
-sendTelegram :: LogConfig -> String -> [RequestParam] -> IO ()
-sendTelegram LogConfig {logLevel = l, logFile = f} url params = do
-  let logManager =
-        if l == Debug
-          then tlsManagerSettings
-                 { managerModifyRequest =
-                     \r ->
-                       appendFile f "\nrequest to server: " >>
-                       appendFile f (show r) >>
-                       return r
-                 , managerModifyResponse =
-                     \r ->
-                       appendFile f "\nresponse status: " >>
-                       appendFile f (show $ responseStatus r) >>
-                       return r
-                 }
-          else tlsManagerSettings
-      body = buildRequestBody params
-  manager <- newManager logManager
-  request <- buildRequest url body
-  response <- httpLbs request manager
-  return ()
+-- sendTelegram :: LogConfig -> String -> [RequestParam] -> IO ()
+-- sendTelegram LogConfig {logLevel = l, logFile = f} url params = do
+--   let logManager =
+--         if l == Debug
+--           then tlsManagerSettings
+--                  { managerModifyRequest =
+--                      \r ->
+--                        appendFile f "\nrequest to server: " >>
+--                        appendFile f (show r) >>
+--                        return r
+--                  , managerModifyResponse =
+--                      \r ->
+--                        appendFile f "\nresponse status: " >>
+--                        appendFile f (show $ responseStatus r) >>
+--                        return r
+--                  }
+--           else tlsManagerSettings
+--       body = buildRequestBody params
+--   manager <- newManager logManager
+--   request <- buildRequest url body
+--   response <- httpLbs request manager
+--   return ()
 
-buildRequest :: String -> RequestBody -> IO Request
-buildRequest url body = do
-  nakedRequest <- parseRequest url
-  return
-    (nakedRequest
-       { method = "POST"
-       , requestBody = body
-       , requestHeaders = [(HTTP.hContentType, "application/json")]
-       })
+-- buildRequest :: String -> RequestBody -> IO Request
+-- buildRequest url body = do
+--   nakedRequest <- parseRequest url
+--   return
+--     (nakedRequest
+--        { method = "POST"
+--        , requestBody = body
+--        , requestHeaders = [(HTTP.hContentType, "application/json")]
+--        })
+stt = (1,2)
