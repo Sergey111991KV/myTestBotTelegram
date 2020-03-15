8a223320117e5158bcac88e3ce56e084c3f686a7
Sergey111991KV <sergey.cosilov@gmail.com>
2020-03-15 23:32:16 +0300

    1

--- app/Main.hs
+++ app/Main.hs
@@ -1,19 +1,12 @@
-{-# LANGUAGE OverloadedStrings #-}
-
-module Main where
-
-import Control.Concurrent.Async
-import           Data.Aeson            (Value)
-import qualified Data.ByteString.Char8 as S8
-import qualified Data.Yaml             as Yaml
-import           Network.HTTP.Simple
-
+{import Network.HTTP.Client
+import Network.HTTP.Types.Status (statusCode)
 
 main :: IO ()
 main = do
-    response <- httpJSON "https://api.telegram.org/bot<823135486:AAHRRGLZFyKAa-hh7bkULT_fLJEn_2xorpg>/"
-    
-    putStrLn $ "The status code was: " ++
-               show (getResponseStatusCode response)
-    print $ getResponseHeader "Content-Type" response
-    S8.putStrLn $ Yaml.encode (getResponseBody response :: Value)
+  manager <- newManager defaultManagerSettings
+
+  request <- parseRequest "http://httpbin.org/get"
+  response <- httpLbs request manager
+
+  putStrLn $ "The status code was: " ++ (show $ statusCode $ responseStatus response)
+  print $ responseBody response
